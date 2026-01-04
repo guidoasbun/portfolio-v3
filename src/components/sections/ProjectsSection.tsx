118fed3c72e5d5faa366ca621c3bac66cf18e6cf
guidoasbun <guido@asbun.io>
2026-01-03 20:34:37 -0800

    database pull

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -27,7 +27,7 @@ export function ProjectsSection({
 
           {/* Grid */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {mockProjects.map((project) => (
+            {projects.map((project) => (
               <ProjectCard key={project.id} project={project} />
             ))}
           </div>
